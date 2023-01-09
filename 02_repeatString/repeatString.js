4ff1be478d4e2b3fb8281eca5d2a67cb77a46a5e
nicklasvsams <nicklasvsams@gmail.com>
2023-01-09 07:52:24 +0100

    Repeat String solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(message, count) {
+    let string = "";
 
+    if (count < 0) return "ERROR";
+    
+    for (let index = 0; index < count; index++) {
+        string += message;
+    }
+
+    return string;
 };
 
 // Do not edit below this line
