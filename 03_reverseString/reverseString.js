d229e37ec71d72d3d5b81bfcb54fecc0087ee418
nicklasvsams <nicklasvsams@gmail.com>
2023-01-09 07:59:42 +0100

    Reverse String solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (text) {
+    var reversedString = "";
 
+    for (var i = text.length; i > 0; i--) {
+        reversedString += text[i - 1];
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
